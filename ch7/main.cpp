f0e4d2db328e707fdeb74c706ffd97f4678e48f8
mooophy <MoophyAlan@gmail.com>
2014-05-24 19:11:46 +1200

    added implementation for randomized_quick_sort 	modified:   .gitignore 	modified:   ch7/main.cpp 	modified:   ch7/randomized_quick_sort.h

--- ch7/main.cpp
+++ ch7/main.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
-#include "quick_sort.h"
 #include <vector>
+#include <algorithm>
 
+#include "quick_sort.h"
+#include "randomized_quick_sort.h"
 
 int main()
 {
-    std::vector<std::string> v = {"13","19","9","5","12","8","7","4","21","2","6","11"};
-    //std::cout << *ch7::partition(v.begin(),v.end()) << std::endl;
+    std::vector<float> v = {5, 3, 2, 6, 9};
 
-    ch7::quick_sort(v.begin(), v.end());
-
+/*  -- uncomment for test
+    //! test quick_sort
+    //ch7::quick_sort(v.begin(), v.end());
+    //! test randomized_quick_sort
+    ch7::randomized_quicksort(v.begin(),v.end());
+*/
     for(auto i : v)
         std::cout << i << " ";
-
     std::cout << std::endl;
 
     return 0;
--- ch7/randomized_quick_sort.h
+++ ch7/randomized_quick_sort.h
@@ -9,11 +9,11 @@
 //! pseudocode
 //! based on RANDOMIZED-PARTITION Page 179
 //!
-/*      randomized_partion(first, last)
+/*      randomized_partition(first, last)
  *1     def random_engine e
  *2     def uniform_distrabution ud(0, last - first - 1)
  *3     def piovt = ud(e)
- *4     swap(*pivot,*(last - 1))
+ *4     swap(*pivot, *(last - 1))
  *5     return partition(first, last)
  */
 
@@ -21,7 +21,6 @@
 //! pseudocode
 //! based on RANDOMIZED-QUICKSORT Page 179
 //!
-
 /*      randomized_quicksort(first, last)
  * 1    if last - first > 1
  * 2        pivot = randomized_partion(first, last)
@@ -33,6 +32,50 @@
 #ifndef RANDOMIZED_QUICK_SORT_H
 #define RANDOMIZED_QUICK_SORT_H
 
+#include "quick_sort.h"
+#include <random>
+#include <assert.h>
+#include <iterator>
+
+namespace ch7
+{
+template<typename Iter>
+Iter randomized_partition(Iter first, Iter last);
+
+template<typename Iter>
+void randomized_quicksort(Iter first, Iter last);
+
+/**
+ * @brief randmized_partition
+ */
+template<typename Iter>
+inline Iter randomized_partition(Iter first, Iter last)
+{
+    assert(last > first);
+    static std::default_random_engine e;
+    static std::uniform_real_distribution<float> ud(0, 100);
+
+    auto range = last - first;
+    Iter pivot = first + ud(e) * range/100;
+    std::swap(*pivot, *(last - 1));
+    return ch7::partition(first, last);
+}
+
+/**
+ * @brief randomized_quicksort
+ */
+template<typename Iter>
+inline void randomized_quicksort(Iter first, Iter last)
+{
+    if(last - first > 1)
+    {
+        Iter pivot = randomized_partition(first, last);
+        randomized_quicksort(first, pivot);
+        randomized_quicksort(pivot + 1, last);
+    }
+}
+
+}//namespace
 
 
 #endif // RANDOMIZED_QUICK_SORT_H
