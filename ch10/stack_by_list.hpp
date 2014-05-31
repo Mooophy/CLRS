5a8557dec2f28dd003c88b5c4a68e9f2435c7465
mooophy <MoophyAlan@gmail.com>
2014-06-01 00:44:01 +1200

    fixed a wrong test code 	modified:   ch10/queue_by_list.hpp 		added test code
    	modified:   ch10/stack_by_list.hpp
    		remove wrong test code

--- ch10/stack_by_list.hpp
+++ ch10/stack_by_list.hpp
@@ -86,21 +86,3 @@ private:
 
 #endif // STACK_BY_LIST_H
 
-
-//! testing code:
-
-//#include <iostream>
-//#include "queue_by_list.h"
-
-//int main()
-//{
-//    ch10::list::queue_by_list<long> q;
-
-//    for(int i = 0; i != 10; ++i)
-//        q.enqueue(i);
-
-//    for(int i = 0; i != 10; ++i)
-//        std::cout << q.dequeue() << std::endl;
-
-//    return 0;
-//}
