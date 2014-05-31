c48b4d0806c4749b218cb5193b637b71b31ac2fc
mooophy <MoophyAlan@gmail.com>
2014-05-31 23:50:38 +1200

    refreshed 	modified:   ch10/main.cpp

--- ch10/main.cpp
+++ ch10/main.cpp
@@ -6,35 +6,7 @@
 
 int main()
 {
-//    ch10::list::single_list_ring<int> l;
-//    l.insert(99);
-//    l.insert(98);
-//    l.insert(9);
-//    l.insert(1);
-//    l.remove(l.begin());
-//    auto ptr = l.search(90);
 
-//    std::cout << l ;
-    ch10::list::stack_by_list<int> stk;
-
-//    stk.enqueue(1);
-
-//    stk.enqueue(2);
-
-//    stk.enqueue(3);
-
-//    stk.dequeue();
-
-//    stk.print();
-    for(int i = 0; i != 10; ++i)
-    {
-        stk.enqueue(i);
-    }
-
-    for(int i = 0; i != 10; ++i)
-    {
-        std::cout << stk.dequeue() << std::endl;
-    }
 
     return 0;
 }
