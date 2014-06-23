cc6b14258208bcfcc60677f0a2f06a37736dc9a8
mooophy <MoophyAlan@gmail.com>
2014-06-24 10:20:09 +1200

    refact insert method based on CLRS  On branch ch12

--- ch12/binary_search_tree.h
+++ ch12/binary_search_tree.h
@@ -4,6 +4,24 @@
  *  @date       24  June 2014
  *  @remark     Implementations for algorithims from CLRS using C++ templates.
  ***************************************************************************/
+//!     page 294
+//!     tree-insert(T,z)
+/* 1    y = nil
+ * 2    x = T.root
+ * 3    whie x != nil
+ * 4        y = x
+ * 5        if z.key < x.key
+ * 6            x = x.left
+ * 7        else
+ *              x = x.right
+ * 8    z.parent = y
+ * 9    if y == nil
+ * 10       T.root = z  //i.e. was empty
+ * 11   elseif z.key <y.key
+ * 12       y.left = z
+ * 13   else    y.right = z
+ */
+
 #ifndef BINARY_SEARCH_TREE_H
 #define BINARY_SEARCH_TREE_H
 
@@ -25,40 +43,49 @@ public:
     using wPointer  =   std::weak_ptr<Node>;
 
     /**
-     * @brief insert interface
+     * @brief insert
+     * @param key
+     * @param data
      *
      * @complexity  O(lg n)
      */
     void insert(const KeyType& key, const DataType& data)
     {
-        sPointer inserted = std::make_shared<Node>(key,data);
-        recur_add(root, inserted);
+        sPointer inserted = std::make_shared<Node>(key, data);
+        insert(inserted);
+    }
+
+    /**
+     * @brief insert
+     * @param inserted  i.e. the new node
+     *
+     * @complexity  O(lg n)
+     */
+    void insert(sPointer inserted)
+    {
+        sPointer last = nullptr;
+        sPointer curr = root;
+
+        //! find the appropiate position
+        while(curr)
+        {
+            last = curr;
+            curr = (inserted->key < curr->key)?  curr->left  :  curr->right;
+        }
+
+        //! graft on the inserted.
+        inserted->parent    =   last;
+        if(!last)
+            root = inserted;
+        else if(inserted->key   <   last->key)
+            last->left      =   inserted;
+        else
+            last->right     =   inserted;
     }
 
 private:
     sPointer root;
-
-    /**
-     * @brief recur_add
-     *
-     * @complexity  O(lg n)
-     */
-    void recur_add(sPointer& curr,const sPointer& added, sPointer prev = nullptr)
-    {
-        if(!curr)
-        {
-            curr            =   added;
-            curr->parent    =   prev;
-        }
-        else
-        {
-            if(added->key   <   curr->key)
-                recur_add(curr->left,   added,  curr);
-            else
-                recur_add(curr->right,  added,  curr);
-        }
-    }
-};
+};//class binary_search_tree
 
 }//namespace ch12
 
