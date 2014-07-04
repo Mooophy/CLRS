cef31dd9c69cc1499ae4dd96fa4c5bd92b70e5c6
mooophy <MoophyAlan@gmail.com>
2014-07-05 11:53:19 +1200

    one more time for ch13 i.e. red black tree  On branch ch13 	new file:   ch13/ch13.pro 	new file:   ch13/debug.hpp 	new file:   ch13/main.cpp

---
+++ ch13/ch13.pro
@@ -0,0 +1,10 @@
+TEMPLATE = app
+CONFIG += console
+CONFIG -= app_bundle
+CONFIG -= qt
+
+SOURCES += main.cpp
+
+HEADERS += \
+    debug.hpp
+
---
+++ ch13/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <debug.hpp>
+
+int main()
+{
+    std::cout << debug::yellow("hello!") << std::endl;
+
+    return 0;
+}
+
