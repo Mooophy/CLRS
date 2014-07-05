6e124d91c327621097a249bad1ca87e80f798493
mooophy <MoophyAlan@gmail.com>
2014-07-05 15:16:52 +1200

    On branch ch13 	modified:   ch13/main.cpp

--- ch13/main.cpp
+++ ch13/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <debug.hpp>
+#include <vector>
 #include <node.hpp>
 #include "redblacktree.hpp"
 
@@ -7,9 +8,10 @@ int main()
 {
     ch13::RedBlackTree<int,std::string> tree;
 
-    tree.insert(3);
-    tree.insert(5);
-    tree.insert(1);
+    std::vector<int> v = {2,3,5,1,6};
+
+    for(auto i : v)
+        tree.insert(i);
 
 
     tree.print();
