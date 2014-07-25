f6c16ce8ade7dd3fa7a2b3c468ed2886a7d38f76
mooophy <MoophyAlan@gmail.com>
2014-07-25 23:27:20 +1200

    added and tested top-down dynamic programming 	modified:   ch15/cut_rod.hpp

--- ch15/cut_rod.hpp
+++ ch15/cut_rod.hpp
@@ -10,15 +10,13 @@
 #include <algorithm>
 #include <iterator>
 #include <limits>
+#include "color.hpp"
 
 namespace ch15 {
 
 template<typename Iter>
 typename std::iterator_traits<Iter>::value_type
 cut_rod(Iter first, typename std::iterator_traits<Iter>::difference_type len);
-
-template<typename Iter>
-class RodCutter;
 
 /**
  * @brief cut_rod
@@ -50,6 +48,8 @@ cut_rod(Iter first, typename std::iterator_traits<Iter>::difference_type len)
     return ret;
 }
 
+
+
 /**
  * @brief The RodCutter class
  *
@@ -63,19 +63,138 @@ public:
     using SizeType  =   typename    std::iterator_traits<Iter>::difference_type;
     using Container =               std::vector<ValueType>;
 
-    ValueType operator()(Iter first, SizeType len)
+    /**
+     * @brief   Ctor
+     * @page    365
+     * essentially, implementing MEMOIZED-CUT-ROD
+     */
+    RodCutter(Iter first, Iter last):
+        revenue(last - first + 1, std::numeric_limits<ValueType>::min())
+    {}
+
+    /**
+     * @brief reset
+     *
+     * call this function when applied on another array.
+     */
+    void reset(Iter first, Iter last)
+    {
+        revenue = Container(first,last);
+    }
+
+    /**
+     * @brief   return the optimal revenue
+     *
+     * @note    just a wraper for virtual functions
+     */
+    ValueType optimize(Iter first, SizeType len)
     {
         return dynamic_program(first, len);
     }
 
-    ~RodCutter(){}
+    /**
+     * @brief print the data stored in buffer.
+     */
+    void print_container() const
+    {
+        std::cout << color::green("The buffer data in container:\n");
+        for(const auto& item : revenue)
+            std::cout << item << " ";
+        std::cout << std::endl;
+    }
+
+    virtual ~RodCutter(){}
 
 protected:
-    Container   vec;
+    Container   revenue;
 
+    /**
+     * @brief the virtual function.
+     */
     virtual ValueType dynamic_program(Iter first, SizeType len) = 0;
 };
 
+/**
+ * @brief top-down dynamic programming for cut_rod
+ */
+template <typename Iter>
+class RodCutterTopDown : public RodCutter<Iter>
+{
+public:
+    using B         =               ch15::RodCutter<Iter>;
+    using SizeType  =   typename    B::SizeType;
+    using ValueType =   typename    B::ValueType;
+
+    /**
+     * @brief Ctor for top-down dynamic programming
+     */
+    RodCutterTopDown(Iter first, Iter last):
+        B(first,last)
+    {}
+
+    virtual ~RodCutterTopDown(){}
+protected:
+
+    using B::revenue;
+
+    /**
+     * @brief the virtual function
+     */
+    virtual ValueType dynamic_program(Iter first, SizeType len) override
+    {
+        std::cout << color::yellow("By top-down dynamic programming:\n");
+        return top_down(first, len);
+    }
+
+private:
+    /**
+     * @brief top_down
+     * @param first the begin iterator for array p
+     * @param len   n
+     *
+     * @page 365
+     * @note essentially implementing MEMOIZED-CUT-ROD-AUX(p,n,r)
+     */
+    ValueType top_down(Iter first, SizeType len)
+    {
+        if(revenue[len] == 0)
+            return revenue[len];
+
+        ValueType ret;
+        if(len == 0)
+            ret = 0;
+        else
+        {
+            ret = std::numeric_limits<ValueType>::min();
+            for(SizeType count = 0 ; count != len ; ++count)
+                ret = std::max(ret, *(first + count) + top_down(first, len - count - 1));
+        }
+
+        return revenue[len] = ret;
+    }
+};
 
 }//namespace
 #endif // CUT_ROD_HPP
+
+//! test for top-down
+//#include <iostream>
+//#include <memory>
+//#include "cut_rod.hpp"
+//#include "color.hpp"
+//int main()
+//{
+//    //! build the array r
+//    std::vector<int> v = {1,5,8,9};
+//    using Iter      =   std::vector<int>::iterator;
+//    using TopDown   =   ch15::RodCutterTopDown<Iter>;
+
+//    //! allocation for the top-down dynamic programming
+//    auto cut =  std::make_shared<TopDown>(v.begin(), v.end());
+
+//    //! print
+//    std::cout << cut->optimize(v.begin(),v.size()) <<std::endl;
+//    cut->print_container();
+
+//    return 0;
+//}
--- ch15/main.cpp
+++ ch15/main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
+#include <memory>
 #include "cut_rod.hpp"
 #include "color.hpp"
 int main()
 {
+    //! build the array r
     std::vector<int> v = {1,5,8,9};
+    using Iter      =   std::vector<int>::iterator;
+    using TopDown   =   ch15::RodCutterTopDown<Iter>;
 
-    std::cout << color::green("test cut_rod:\n");
-    std::cout << ch15::cut_rod(v.begin(), v.size()) << std::endl;
+    //! allocation for the top-down dynamic programming
+    auto cut =  std::make_shared<TopDown>(v.begin(), v.end());
+
+    //! print
+    std::cout << cut->optimize(v.begin(),v.size()) <<std::endl;
+    cut->print_container();
 
     return 0;
 }
