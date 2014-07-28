9ac68296181e8c67970aad7cdd51aae7ff2797db
mooophy <MoophyAlan@gmail.com>
2014-07-28 20:43:08 +1200

    update 	modified:   ch15/cut_rod.hpp

--- ch15/cut_rod.hpp
+++ ch15/cut_rod.hpp
@@ -383,3 +383,20 @@ protected:
 //    return 0;
 //}
 
+//! test ex15.1-3 : cut with cost
+//#include <iostream>
+//#include <memory>
+//#include "cut_rod.hpp"
+//#include "color.hpp"
+//int main()
+//{
+//    //! build the array r
+//    std::vector<int> v = {1,5,8,9,10};
+
+//    int result = ch15::bottom_up_with_cost(v.begin(), v.end(), 1);
+//    std::cout << result << std::endl;
+
+
+//    std::cout << color::red("\nend\n");
+//    return 0;
+//}
--- ch15/main.cpp
+++ ch15/main.cpp
@@ -8,11 +8,7 @@ int main()
     std::vector<int> v = {1,5,8,9,10};
 
     int result = ch15::bottom_up_with_cost(v.begin(), v.end(), 1);
-
     std::cout << result << std::endl;
-
-
-
 
 
     std::cout << color::red("\nend\n");
