baf329d14da64dab4690bb819976f3ca8ba894ee
mooophy <MoophyAlan@gmail.com>
2014-07-31 01:14:14 +1200

    working on matrix chain multification  On branch master 	modified:   ch15/matrix.hpp

--- ch15/main.cpp
+++ ch15/main.cpp
@@ -4,7 +4,24 @@
 
 int main()
 {
-    ch15::Matrix<int> mat;
+    ch15::Matrix<int> lhs(2,2), rhs(2,2);
+
+    for(unsigned r = 0; r != lhs.size1(); ++r)
+        for(unsigned c = 0; c != lhs.size2(); ++c)
+            lhs(r,c)    =   r + c;
+
+    for(unsigned r = 0; r != rhs.size1(); ++r)
+        for(unsigned c = 0; c != rhs.size2(); ++c)
+            rhs(r,c)    =   r + c;
+
+
+
+    auto ret = ch15::operator *(lhs,rhs);
+
+    ch15::print(ret);
+
+
+
 
 
 
--- ch15/matrix.hpp
+++ ch15/matrix.hpp
@@ -8,6 +8,7 @@
 #define MATRIX_HPP
 
 #include <boost/numeric/ublas/matrix.hpp>
+#include <assert.h>
 
 namespace ch15
 {
@@ -18,15 +19,33 @@ template<typename T>
 Matrix<T>
 operator*(const Matrix<T>& lhs, const Matrix<T>& rhs);
 
+template<typename T>
+void print(const Matrix<T>& mat)
+{
+    for(unsigned r = 0; r != mat.size1(); ++r)
+        for(unsigned c = 0; c != mat.size2(); ++c)
+            std::cout << mat(r,c) << std::endl;
+}
 
 template<typename T>
 inline Matrix<T>
 operator*(const Matrix<T>& lhs, const Matrix<T>& rhs)
 {
+    assert(lhs.size2() == rhs.size1());
 
+    Matrix<T> ret(lhs.size1(), rhs.size2());
+    for(unsigned row = 0; row != lhs.size1(); ++row)
+        for(unsigned col = 0; col != rhs.size2(); ++ col)
+        {
+            ret(row,col) = 0;
+            for(unsigned count = 0 ; count != lhs.size2(); ++count)
+                ret(row,col) += lhs(row,count) * rhs(count, col);
+        }
+
+    return ret;
 }
 
-}
+}//namespace
 
 
 #endif // MATRIX_HPP
