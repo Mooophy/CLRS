7f24c6438a228a8b78a60e6722fd93c24a108235
mooophy <MoophyAlan@gmail.com>
2014-08-07 23:30:02 +1200

    update test code for matrix chain order 	modified:   ch15/matrix_chain_order.hpp

--- ch15/main.cpp
+++ ch15/main.cpp
@@ -1,22 +1,18 @@
 #include <iostream>
-#include <boost/numeric/ublas/matrix.hpp>
-#include <boost/numeric/ublas/io.hpp>
-#include "color.hpp"
 #include "matrix.hpp"
+#include "color.hpp"
 #include "matrix_chain_order.hpp"
-#include "matrix_chain_mutiply.hpp"
 
 int main()
 {
-    using namespace boost::numeric::ublas;
+    std::vector<int> v = {30,35,15,5,10,20,25};
+    ch15::MatrixChainOrder<decltype(v)> chain_order(v);
 
-    std::vector<int> v = {1,3,3,4,5,6,7,8,9};
-    matrix<int> mat(3,3);
+    chain_order.build();
+    chain_order.print();
 
-    v >>= mat;
-    std::cout << mat << std::endl;
+    chain_order.print_optimal(1,6);
 
     std::cout << color::red("\nend\n");
     return 0;
 }
-
--- ch15/matrix_chain_order.hpp
+++ ch15/matrix_chain_order.hpp
@@ -138,17 +138,17 @@ private:
 //#include <iostream>
 //#include "matrix.hpp"
 //#include "color.hpp"
-//#include "matrix_chain.hpp"
+//#include "matrix_chain_order.hpp"
 
 //int main()
 //{
 //    std::vector<int> v = {30,35,15,5,10,20,25};
-//    ch15::MatrixChain<decltype(v)> chain(v);
+//    ch15::MatrixChainOrder<decltype(v)> chain_order(v);
 
-//    chain.build();
-//    chain.print();
+//    chain_order.build();
+//    chain_order.print();
 
-//    chain.print_optimal(1,6);
+//    chain_order.print_optimal(1,6);
 
 //    std::cout << color::red("\nend\n");
 //    return 0;
