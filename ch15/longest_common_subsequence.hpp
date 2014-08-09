83d859c3900e0a66f465a062f4b2403925214511
mooophy <MoophyAlan@gmail.com>
2014-08-09 19:07:49 +1200

    upgrade Table and Entry 	modified:   ch15/longest_common_subsequence.hpp

--- ch15/longest_common_subsequence.hpp
+++ ch15/longest_common_subsequence.hpp
@@ -9,6 +9,7 @@
 #define LONGEST_COMMON_SUBSEQUENCE_HPP
 
 #include "matrix.hpp"
+#include "color.hpp"
 
 namespace ch15 {
 
@@ -20,23 +21,49 @@ enum class Arrow
 template<typename T, typename A>
 struct Entry
 {
-    //! default Ctor
+    //! Ctors
     Entry() = default;
-
-    //! Ctor
-    Entry(const T& len, const A& arw):
-        lengh(len), arrow(arw)
+    Entry(const T& len, const A& arw = Arrow::NA):
+        length(len), arrow(arw)
     {}
 
-    T lengh = 0;
+    //! update this entry
+    Entry operator ()(const T& len, const A& arw = Arrow::NA)
+    {
+        length  =   len;
+        arrow   =   arw;
+
+        return *this;
+    }
+
+    T length = 0;
     A arrow = Arrow::NA;
 };
+
+template<typename T, typename A>
+inline std::ostream&
+operator <<(std::ostream& os , const Entry<T,A>& etr)
+{
+    os << color::green("[") << etr.length << ",";
+
+    switch (etr.arrow)
+    {
+    case Arrow::NA      :   os << "N";  break;
+    case Arrow::LEFT    :   os << "L";  break;
+    case Arrow::DIAGONAL:   os << "D";  break;
+    case Arrow::UP      :   os << "U";  break;
+    }
+
+    os << color::green("]");
+
+    return os;
+}
 
 template<typename T, typename A>
 inline bool
 operator >=(const Entry<T,A>& lhs, const Entry<T,A>& rhs)
 {
-    return lhs.lengh >= rhs.lengh;
+    return lhs.length >= rhs.length;
 }
 
 //! @alias for Table
@@ -52,6 +79,7 @@ template<typename Range, typename A = Arrow>
 Table<typename Range::size_type, A>
 build_lcs_table(const Range& lhs, const Range& rhs)
 {
+    //! types def
     using SizeType  =   typename Range::size_type;
     using TableType =   Table<SizeType, A>;
     using EntryType =   Entry<SizeType, A>;
@@ -66,11 +94,11 @@ build_lcs_table(const Range& lhs, const Range& rhs)
         for(SizeType c = 1; c != cols + 1; ++c)
         {
             if(lhs[r - 1]   ==  rhs[c - 1])
-                lcs(r,c) = EntryType(ret(r - 1, c - 1).length + 1,  Arrow::DIAGONAL);
-            else if(ret(r - 1, c) >= ret(r, c - 1))
-                lcs(r,c) = EntryType(ret(r - 1, c).length,  Arrow::UP);
+                lcs(r,c) = EntryType(lcs(r - 1, c - 1).length + 1,  Arrow::DIAGONAL);
+            else if(lcs(r - 1, c) >= lcs(r, c - 1))
+                lcs(r,c) = EntryType(lcs(r - 1, c).length,  Arrow::UP);
             else
-                lcs(r,c) = EntryType(ret(r, c - 1).length,  Arrow::LEFT);
+                lcs(r,c) = EntryType(lcs(r, c - 1).length,  Arrow::LEFT);
         }
 
     return lcs;
@@ -79,6 +107,22 @@ build_lcs_table(const Range& lhs, const Range& rhs)
 }//namespace
 #endif // LONGEST_COMMON_SUBSEQUENCE_HPP
 
+//! @test   print ch15::table
+//#include <iostream>
+//#include <boost/numeric/ublas/io.hpp>
+//#include "longest_common_subsequence.hpp"
+//#include "color.hpp"
+
+//int main()
+//{
+//    using TableType = ch15::Table<int, ch15::Arrow>;
+
+//    TableType tbl(3,3, ch15::Entry<int, ch15::Arrow>());
+//    std::cout << tbl << std::endl;
+
+//    std::cout << color::red("\nend\n");
+//    return 0;
+//}
 
 
 
--- ch15/main.cpp
+++ ch15/main.cpp
@@ -5,6 +5,12 @@
 
 int main()
 {
+    std::vector<int> lhs = {1,5,6,7,9};
+    std::vector<int> rhs = {6,7,9};
+
+    auto ret = ch15::build_lcs_table(lhs, rhs);
+    std::cout << ret << std::endl;
+
     std::cout << color::red("\nend\n");
     return 0;
 }
