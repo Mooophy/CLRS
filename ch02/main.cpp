52c60c0ac501dfb2a268e2f172b5349b068e75f7
mooophy <MoophyAlan@gmail.com>
2014-08-24 16:07:44 +1200

    modified:   selection_sort.hpp 	: 	done

--- ch02/main.cpp
+++ ch02/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <vector>
 #include "alan.hpp"
-#include "binary_search.hpp"
+#include "selection_sort.hpp"
+
 int main()
 {
-    std::vector<int> v{1,2,3,4,5,6,32,99};
-
-    auto ret = clrs::ch2::is_sum(v.begin(), v.end(), 100);
-    std::cout << ret;
+    std::vector<int> v{1,6,88,2,0,77};
+    clrs::ch2::selection_sort(v.begin(), v.end());
+    alan::print_container(v);
 
     alan::end();
     return 0;
--- ch02/selection_sort.hpp
+++ ch02/selection_sort.hpp
@@ -7,9 +7,8 @@
  ***************************************************************************/
 
 //!
-//! ex 2.2-2 Selection-Sort
+//! ex2.2-2 Selection-Sort
 //!
-
 
 #ifndef SELECTION_SORT_HPP
 #define SELECTION_SORT_HPP
@@ -18,9 +17,18 @@
 
 namespace clrs {namespace ch2 {
 
+/**
+ * @brief selection_sort
+ * @param first
+ * @param last
+ *
+ * @ex2.2-2
+ * @complx  O(n^2)
+ */
 template<typename Iter>
 void selection_sort(Iter first, Iter last)
 {
+    //! define a lambda to find the minimum element
     using Lamda =   std::function<Iter(Iter,Iter)>;
     Lamda min   =   [](Iter first, Iter last)->Iter
     {
@@ -30,10 +38,31 @@ void selection_sort(Iter first, Iter last)
         return ret;
     };
 
+    //! sort with lambda min()
     for(auto it = first; it != last - 1; ++it)
         std::swap(*it, *min(it, last));
 }
 
-
 }}//namespace
 #endif // SELECTION_SORT_HPP
+
+//! @test   selection_sort for ex2.2-2
+//!
+//#include <iostream>
+//#include <vector>
+//#include "alan.hpp"
+//#include "selection_sort.hpp"
+
+//int main()
+//{
+//    std::vector<int> v{1,6,88,2,0,77};
+//    clrs::ch2::selection_sort(v.begin(), v.end());
+//    alan::print_container(v);
+
+//    alan::end();
+//    return 0;
+//}
+//! @output
+//!
+//0 1 2 6 77 88
+//exit normally
