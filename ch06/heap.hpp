fab24b211bcd883ff43a7d03790f6ad9a28438fa
mooophy <MoophyAlan@gmail.com>
2014-08-30 23:45:14 +1200

    refactor : max_heapify 	modified:   heap.hpp

--- ch06/heap.hpp
+++ ch06/heap.hpp
@@ -14,30 +14,50 @@ namespace clrs { namespace ch6 {
  * @brief parent
  */
 template<typename SizeType>
-inline SizeType parent (SizeType index)
+inline SizeType parent(SizeType target)
 {
-    return (index - 1)/2;
+    return (target - 1)/2;
 }
 
 /**
  * @brief left
  */
 template<typename SizeType>
-inline SizeType left (SizeType index)
+inline SizeType left(SizeType index)
 {
-    return 2 * index + 1;
+    return 2 * target + 1;
 }
 
 /**
  * @brief right
  */
 template<typename SizeType>
-inline SizeType right (SizeType index)
+inline SizeType right(SizeType index)
 {
-    return 2 * index + 2;
+    return 2 * target + 2;
 }
 
+/**
+ * @brief max_heapify
+ * @param rng
+ * @param curr
+ */
+template<typename Range>
+void max_heapify(Range& rng, typename Range::size_type curr)
+{
+    auto l = left(curr);
+    auto largest = (l < rng.size()  &&  rng[l] > rng[curr])?  l   :   curr;
 
-}}
+    auto r = right(curr);
+    if(r < rng.size()   &&  rng[r] > rng[largest])  largest = r;
+
+    if(largest != curr)
+    {
+        std::swap(rng[largest], rng[curr]);
+        max_heapify(rng, largest);
+    }
+}
+
+}}//namespace
 
 #endif // HEAP_HPP
