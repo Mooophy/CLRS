78e07bdecf0538e414866bbec76be6e74a6dbfdb
mooophy <MoophyAlan@gmail.com>
2014-08-31 10:29:43 +1200

    new file:   misc/alan.hpp 	new file:   misc/iterator.hpp

--- ch06/heap.hpp
+++ ch06/heap.hpp
@@ -23,7 +23,7 @@ inline SizeType parent(SizeType target)
  * @brief left
  */
 template<typename SizeType>
-inline SizeType left(SizeType index)
+inline SizeType left(SizeType target)
 {
     return 2 * target + 1;
 }
@@ -32,7 +32,7 @@ inline SizeType left(SizeType index)
  * @brief right
  */
 template<typename SizeType>
-inline SizeType right(SizeType index)
+inline SizeType right(SizeType target)
 {
     return 2 * target + 2;
 }
@@ -41,16 +41,23 @@ inline SizeType right(SizeType index)
  * @brief max_heapify
  * @param rng
  * @param curr
+ *
+ * @pseudocode MAX-HEAPIFY, Page 154
+ * @recurrence T(n) <= T(2n/3) + theta(1)
+ * @complx  O(lg n)
  */
 template<typename Range>
 void max_heapify(Range& rng, typename Range::size_type curr)
 {
+    //! compare with left child
     auto l = left(curr);
     auto largest = (l < rng.size()  &&  rng[l] > rng[curr])?  l   :   curr;
 
+    //! compare with right child
     auto r = right(curr);
     if(r < rng.size()   &&  rng[r] > rng[largest])  largest = r;
 
+    //! recur when necessary
     if(largest != curr)
     {
         std::swap(rng[largest], rng[curr]);
@@ -59,5 +66,25 @@ void max_heapify(Range& rng, typename Range::size_type curr)
 }
 
 }}//namespace
-
 #endif // HEAP_HPP
+
+//! @test
+//!
+//#include <vector>
+//#include <iostream>
+//#include "../misc/alan.hpp"
+//#include "heap.hpp"
+
+//int main()
+//{
+//    std::vector<int> v {16,4,10,14,7,9,3,2,8,1};
+//    clrs::ch6::max_heapify(v,1);
+//    alan::print_container(v);
+
+//    alan::end();
+//    return 0;
+//}
+//! @output
+//!
+//16 14 10 8 7 9 3 2 4 1
+//exit normally
--- ch06/main.cpp
+++ ch06/main.cpp
@@ -1,8 +1,15 @@
 #include <vector>
 #include <iostream>
+#include "../misc/alan.hpp"
+#include "heap.hpp"
 
 int main()
 {
+    std::vector<int> v {16,4,10,14,7,9,3,2,8,1};
+    clrs::ch6::max_heapify(v,1);
+    alan::print_container(v);
+
+    alan::end();
     return 0;
 }
 
