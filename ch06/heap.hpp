a731bb00050f86a6272d0efeb24a77e707a004e3
mooophy <MoophyAlan@gmail.com>
2014-08-31 19:21:22 +1200

    add heapify. 	modified:   ch06/heap.hpp

--- ch06/heap.hpp
+++ ch06/heap.hpp
@@ -9,9 +9,45 @@
 #define HEAP_HPP
 
 #include <functional>
+#include "../misc/iterator.hpp"
 
 namespace clrs { namespace ch6 {
 
+template<typename Iter>
+inline Iter parent(Iter first, Iter curr)
+{
+    return first + (curr - first - 1)/ 2;
+}
+
+template<typename Iter>
+inline Iter left(Iter first, Iter curr)
+{
+    return first + 2 * (curr - first) + 1;
+}
+
+
+template<typename Iter>
+inline Iter right(Iter first, Iter curr)
+{
+    return first + 2 * (curr - first) + 2;
+}
+
+template<typename Iter, typename Comp = std::greater<IterValue<Iter> > >
+void heapify(Iter first, Iter last, Iter curr, Comp compare = Comp())
+{
+    Iter l = left(first, curr), r = right(first, curr);
+
+    auto max_min = (l < last    &&  compare(*l, *curr))?    l   :   curr;
+    if(r < last     &&  compare(*r, *max_min))
+            max_min = r;
+
+    if(max_min != curr)
+    {
+        std::swap(*max_min, *curr);
+        heapify(first, last, max_min, Comp());
+    }
+
+}
 
 }}//namespace
 #endif // HEAP_HPP
--- ch06/main.cpp
+++ ch06/main.cpp
@@ -1,14 +1,13 @@
 #include <vector>
 #include <iostream>
 #include "../misc/alan.hpp"
-#include "heap_sort.hpp"
 #include "heap.hpp"
 
 int main()
 {
     std::vector<int> v {1,4};
 //    clrs::ch6::heap_sort(v);
-    clrs::ch6::max_heapify(v.begin(), v.end(), v.begin() + 1);
+    clrs::ch6::heapify(v.begin(), v.end(), v.begin() + 1);
     alan::print_container(v);
 
     alan::end();
