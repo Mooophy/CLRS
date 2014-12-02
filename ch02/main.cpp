83c4d732b2c568f99c38173fb4c76046284d35b2
mooophy <MoophyAlan@gmail.com>
2014-12-02 23:36:19 +1300

    modified:   ch02/main.cpp

--- ch02/main.cpp
+++ ch02/main.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::vector<std::string> v{"99","22","z55","s11","b33"};
+    std::vector<int> v{3,2,1};
 
     clrs::ch2::merge_sort(v.begin(), v.end());
     alan::print_container(v);
