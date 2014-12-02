365bac60816cbe6ee140f398be99f7a004437df0
mooophy <MoophyAlan@gmail.com>
2014-12-02 22:19:24 +1300

    refactor 	modified:   ch02/merge_sort.hpp

--- ch02/main.cpp
+++ ch02/main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <vector>
 #include "alan.hpp"
-#include "inversions.hpp"
+#include "merge_sort.hpp"
 
 int main()
 {
-    std::vector<int> v{2,3,8,6,1};
-    auto count = clrs::ch2::inversions_count(v.begin(), v.end());
-    std::cout << count;
+    std::vector<std::string> v{"99","22","z55","s11","b33"};
+
+    clrs::ch2::merge_sort(v.begin(), v.end());
+    alan::print_container(v);
 
     alan::end();
     return 0;
--- ch02/merge_sort.hpp
+++ ch02/merge_sort.hpp
@@ -1,8 +1,9 @@
 /***************************************************************************
- *  @file       insertion_sort.hpp
- *  @author     Alan.W
+ *  @file       merge_sort.hpp
+ *  @author     Yue.Wang
  *  @date       23  Aug 2014
- *  @version    2
+ *  @update     02  Dec 2014
+ *  @version    3
  *  @remark     CLRS Algorithms implementation in C++ templates.
  ***************************************************************************/
 #ifndef MERGE_SORT_HPP
@@ -11,6 +12,7 @@
 #include <vector>
 #include "iterator.hpp"
 #include <iostream>
+
 
 namespace clrs {namespace ch2 {
 
@@ -27,27 +29,19 @@ namespace clrs {namespace ch2 {
 template<typename Iter>
 void merge(Iter first, Iter mid, Iter last )
 {
-    using Vector    =   std::vector<clrs::IterValue<Iter>>;
-    Vector left{first, mid};
-    Vector right{mid, last};
-
-    auto l = left.begin();
-    auto r = right.begin();
+    //! init
+    using Vector = std::vector<clrs::IterValue<Iter>>;
+    Vector left(first, mid), right(mid, last);
+    auto l = left.cbegin(), r = right.cbegin();
     auto it = first;
-    while(l != left.end()   &&  r != right.end()    &&  it != last)
-    {
-        if(*r < *l)     *it++ = *r++;
-        else            *it++ = *l++;
-    }
 
-    //! when left exausted
-    if(l == left.end())
-        while(it != last)   *it++ = *r++;
+    //! copy until either one reaches its end
+    for(; l != left.end() and r != right.end();  *it++ = *(*r < *l ? r : l)++);
 
-    //! when right exausted
-    if(r == right.end())
-        while(it != last)   *it++ = *l++;
+    //! copy the rest.
+    for(auto rest = (l == left.cend() ? r : l);  it != last;  *it++ = *rest++);
 }
+
 
 /**
  * @brief merge_sort
@@ -65,15 +59,14 @@ void merge_sort(Iter first, Iter last)
         auto mid = first + (last - first)/2;
         merge_sort(first, mid );
         merge_sort(mid  , last);
-
         merge(first, mid, last);
     }
 }
 
 
 }}//namespace
-
 #endif // MERGE_SORT_HPP
+
 
 //! @test   merge sort
 //!
