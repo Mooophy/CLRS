3cafcc7fecf8823223bdd82cefcc8b2c5c053f85
mooophy <MoophyAlan@gmail.com>
2014-12-20 17:46:00 +1300

    working:   adjacency_matrix.hpp

--- ch22/adjacency_matrix.hpp
+++ ch22/adjacency_matrix.hpp
@@ -11,7 +11,38 @@ using std::vector;
 namespace clrs {namespace ch22 {
 
 template<typename Key>
-struct Matrix : public vector<vector<Key> >{};
+class Matrix : public vector<vector<Key> >
+{
+    using Super = vector<vector<Key> >;
+
+public:
+};
+
+
+template<typename Key, typename Data>
+class AdjMatrix
+{
+    using V = Vertex<Key,Data>;
+    using E = Edge<Key,Data>;
+public:
+    using SizeType = typename vector<V>::size_type;
+
+    AdjMatrix() = default;
+
+    void add_vertex(V const& vertex)
+    {
+        Key key = vertex.key_;
+        data_.push_back(vertex);
+    }
+
+    SizeType    size()  const   {return data_.size();   }
+    bool        empty() const   {return data_.empty();  }
+
+
+private:
+    Matrix<Key> mat_;
+    vector<V> data_;
+};
 
 }}//namespace
 #endif // ADJACENCY_MATRIX_HPP
--- ch22/test_for_adjacency_matrix.cpp
+++ ch22/test_for_adjacency_matrix.cpp
@@ -2,9 +2,15 @@
 #include <iostream>
 
 using namespace std;
+using AdjMatrix = clrs::ch22::AdjMatrix<int, std::string>;
 
 int main()
 {
+
+    AdjMatrix g;
+    cout << g.empty() << endl;
+    cout << g.size()  << endl;
+
     cout << "exit" << endl;
     return 0;
 }
