038176da6310bd787372188321faefecc1d88da1
mooophy <MoophyAlan@gmail.com>
2014-12-20 17:00:29 +1300

    working on new file:   adjacency_matrix.hpp

---
+++ ch22/adjacency_matrix.hpp
@@ -0,0 +1,20 @@
+#ifndef ADJACENCY_MATRIX_HPP
+#define ADJACENCY_MATRIX_HPP
+
+
+#include <vector>
+#include "vertex_and_edge.hpp"
+using std::vector;
+
+
+
+namespace clrs {namespace ch22 {
+
+template<typename Key>
+struct Matrix : public vector<vector<Key> >
+{
+
+};
+
+}}//namespace
+#endif // ADJACENCY_MATRIX_HPP
