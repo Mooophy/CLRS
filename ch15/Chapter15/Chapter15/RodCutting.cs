f754bef72c420f2ce250f527c30339c57ac9041a
moophy <MoophyAlan@gmail.com>
2016-08-11 22:02:19 +1200

    added the O(2^n) CutRod

---
+++ ch15/Chapter15/Chapter15/RodCutting.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Chapter15
+{
+    namespace RodCutting
+    {
+        public static class Algorithms
+        {
+            /// <summary>
+            /// O(2^n)
+            /// </summary>
+            /// <param name="p">Prices</param>
+            /// <param name="n">Length</param>
+            /// <returns></returns>
+            public static int CutRod(IList<int> p, int n)
+            {
+                return n == 0 ? 0 : Enumerable
+                    .Range(1, n)
+                    .Aggregate(
+                        int.MinValue, 
+                        (q, i) => Math.Max(q, p[i - 1] + CutRod(p, n - i))
+                    );
+            }
+        }
+    }
+}
---
+++ ch15/Chapter15/UnitTest/RodCuttingTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Chapter15.RodCutting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class RodCuttingTests
+    {
+        [TestMethod]
+        public void CutRod()
+        {
+            var ps = new[] { 1, 5, 8, 9, 10, 17, 17, 20, 24, 30 };
+            Assert.AreEqual(30, Algorithms.CutRod(ps, ps.Length - 0));
+            Assert.AreEqual(25, Algorithms.CutRod(ps, ps.Length - 1));
+            Assert.AreEqual(22, Algorithms.CutRod(ps, ps.Length - 2));
+            Assert.AreEqual(18, Algorithms.CutRod(ps, ps.Length - 3));
+        }
+    }
+}
